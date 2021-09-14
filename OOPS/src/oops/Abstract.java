e5d2b8f8c895fcaa35e364b3f0b35682b3eb71b4
venky428 <venkyvenky428@gmail.com>
2021-09-14 22:12:42 +0530

    OOPS first

---
+++ OOPS/src/oops/Abstract.java
@@ -0,0 +1,23 @@
+package oops;
+
+public abstract class Abstract implements Interface 
+{
+	
+	public abstract void mani();
+	
+	public void venky6()
+	{
+		System.out.println("venky ready for interview");
+	}
+	
+	
+               
+	
+	
+	
+	
+	
+	
+	
+	
+}
